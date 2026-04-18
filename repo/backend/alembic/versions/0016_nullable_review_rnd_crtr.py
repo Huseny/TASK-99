dc0bface27e775d1ccf22ccf2089726c1d2b8638
Husen <husenyusuf877@gmail.com>
2026-04-18 13:31:53 +0300

    feat: allow nullable created_by field in review_rounds table

--- repo/backend/alembic/versions/0016_nullable_review_rnd_crtr.py
+++ repo/backend/alembic/versions/0016_nullable_review_rnd_crtr.py
@@ -1,6 +1,6 @@
 """allow nullable review_rounds.created_by
 
-Revision ID: 0016_nullable_review_round_creator
+Revision ID: 0016_nullable_review_rnd_crtr
 Revises: 0015_integration_actor_users
 Create Date: 2026-04-18 12:00:00
 """
@@ -8,7 +8,7 @@ Create Date: 2026-04-18 12:00:00
 import sqlalchemy as sa
 from alembic import op
 
-revision = "0016_nullable_review_round_creator"
+revision = "0016_nullable_review_rnd_crtr"
 down_revision = "0015_integration_actor_users"
 branch_labels = None
 depends_on = None
